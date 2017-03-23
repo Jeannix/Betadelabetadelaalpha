8851dbcef8d2ecbc8e8d8c958c0a9de2cd0f9bc5
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2017-03-23 15:51:57 -0500

    Operaciones

---
+++ prueba/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a;
+    cout<<"Ingrese el valor de a: ";
+    cin >> a;
+    float b;
+    cout<<"Ingrese el valor de b: ";
+    cin >> b;
+    float d;
+    cout<<"Ingrese el valor del radio d: ";
+    cin>> d;
+    float e;
+    float pi = 3.1416;
+    float c=a+b;
+    cout << "La Suma de "<< a <<" y de "<< b <<" es: "<< c << endl;
+    c=a-b;
+    cout << "La Resta de "<< a <<" y de "<< b <<" es: "<< c << endl;
+    c=a*b;
+    cout << "La Multiplicacion de "<< a <<" y de "<< b <<" es: "<< c << endl;
+    c=a/b;
+    cout << "La Division de "<< a <<" y de "<< b <<" es: "<< c << endl;
+    cout << "El diametro es: " << 2*d << endl;
+    cout << "El area del circulo es:" << pi*(d*d) << endl;
+    cout << "Ingrese la altura del rectangulo: ";
+    cin>> d;
+    cout << "Ingrese la base del rectangulo: ";
+    cin>> e;
+    cout << "El area del rectangulo es: " << e*d << endl;
+    cout << "GG EZ PROFE" << endl;
+    cout << "okno no me jale profe :'v" << endl;
+    return 0;
+}
