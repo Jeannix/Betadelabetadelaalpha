eb3d01cff932a25377a3e5afa6594f9de3b47205
Jean C <jean_16jean@hotmail.com>
2017-06-15 23:46:23 -0500

    Stacks.v2

--- Stocks/main.cpp
+++ Stocks/main.cpp
@@ -3,13 +3,16 @@
 
 using namespace std;
 
+template < class T> class Stack ;
+template < class T> Stack <T> operator +( const Stack <T> &s1 , const Stack <T> &s2);
+template < class T> Stack <T> operator -( const Stack <T> &s1 , const Stack <T> &s2);
+
 template < class T>
 class Stack {
-    friend Stack <T> operator +<>(const Stack <T> &s1 , const Stack <T> &s2);
-    friend Stack <T> operator -<>(const Stack <T> &s1 , const Stack <T> &s2);
-    vector <T> items;
-
-public:
+    friend Stack <T> operator +<>( const Stack <T> &s1 , const Stack <T> &s2);
+    friend Stack <T> operator -<>( const Stack <T> &s1 , const Stack <T> &s2);
+    vector <T> items ;
+public :
     bool empty () const { return items . empty () ;}
     void push ( const T & item ) { items . push_back ( item );}
     T pop () {
@@ -17,34 +20,39 @@ public:
         items . pop_back ();
         return last ;
     }
+    void getDatos(const Stack <T> &s1){
+        for ( unsigned i = 0; i < s1. items . size (); ++i) {
+            cout <<s1. items [i]<<" ";
+
+        }
+    }
 };
 template < class T>
 Stack <T> operator +( const Stack <T> &s1 , const Stack <T> &s2)
-{
+    {
     Stack <T> result = s1;
-
-    for (unsigned i = 0; i < s1.items.size (); ++i) {
-        result.items.push_back (s2. items [i]);
+    for ( unsigned i = 0; i < s1. items . size (); ++i) {
+        result . items . push_back (s2. items [i]);
     }
-
-return result ;
+    return result ;
 }
 template < class T>
-Stack <T> operator -( const Stack <T> &s1 , const Stack <T> &s2)
-{
-    Stack <T> result = s1;
-
-    for (unsigned i = 0; i < s1.items.size (); ++i) {
-        if (s1.items[i]==s2.items[i])
-        {
-            result.items.pop_back (s2. items [i]);
-        }
-    }
-
-return result ;
+Stack <T> operator -( const Stack<T> &s1 , const Stack<T> &s2)
+    {
+	Stack<T> result=s1;
+	int tam = s1.st.size()-1;
+	for(tam;tam>=0;tam--){
+		if(result.st[tam]== s2.st[tam])
+			result.st.erase(result.st.begin()+tam);
+	}
+	return result ;
 }
+int main()
+{
+    Stack<int> stack1, stack2,stackres;
+    stack1.push(8);
+    stack2.push(5);
+    stackres = (stack1+stack2);
+    stackres.getDatos(stackres);
 
-int main(){
-
-    return 0;
 }
